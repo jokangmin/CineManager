87bb67178d26f906a5b60b0e51959fc360864ddd
hyejin <srin5@naver.com>
2024-07-30 16:30:54 +0900

    index main commit

--- src/cineManager/main/IndexMain.java
+++ src/cineManager/main/IndexMain.java
@@ -1,10 +1,49 @@
 package cineManager.main;
 
+import java.util.Scanner;
+
+import member.service.Member;
+import member.service.SignInMember;
+import member.service.SignUpMember;
 public class IndexMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		public void menu() {
+			Scanner in = new Scanner(System.in);
+			int num;
+			Member member = null;
+
+			while(true) {
+				System.out.println();
+				System.out.println("************************");
+				System.out.println("	1. 회원가입");
+				System.out.println("	2. 로그인");
+				System.out.println("	3. CineManager 종료");
+				System.out.println("************************");
+				System.out.print("번호 입력 : ");
+				num = in.nextInt();
+
+				if (num == 1) {
+				member = new SignUpMember();
+				member.execute();
+			} else if (num == 2) {
+				member = new SignInMember();
+				member.execute();
+			} else if (num == 3) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			} else {
+				System.out.println("잘못된 선택입니다. 다시 선택해주세요.");
+			}	
+				
+			}
+		}
+
+	public static void main(String[] args) {
+		IndexMain indexMain = new IndexMain();
+		indexMain.menu();
+		System.out.println("프로그램을 종료합니다");
 	}
 
 }
+
