ead880166db346a8751609302a68ec3a89bf38a5
p3chaeyeon <p3chaeyeon@gmail.com>
2024-07-31 16:19:28 +0900

    fix : mac 배려 MovieDTO 롬복 사용 X

--- src/cineManager/bean/MovieDTO.java
+++ src/cineManager/bean/MovieDTO.java
@@ -1,9 +1,9 @@
 package cineManager.bean;
 
 import java.util.Date;
-import lombok.Data;
+//import lombok.Data;
 
-@Data
+//@Data
 public class MovieDTO {
     private int code; 
     private String title;
@@ -11,5 +11,41 @@ public class MovieDTO {
     private String genre;
     private String releaseDate;
     private String synopsis;
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDirector() {
+		return director;
+	}
+	public void setDirector(String director) {
+		this.director = director;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+	public String getSynopsis() {
+		return synopsis;
+	}
+	public void setSynopsis(String synopsis) {
+		this.synopsis = synopsis;
+	}
 	
 }
