4e2c802ec2a3e97e6e2d965b2fea27b549390655
jokangmin <jomin5151@gmail.com>
2024-08-02 16:52:33 +0900

    indexmain 예외처리

--- src/cineManager/main/IndexMain.java
+++ src/cineManager/main/IndexMain.java
@@ -1,5 +1,6 @@
 package cineManager.main;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import member.service.Member;
 import member.service.SignInMember;
@@ -7,9 +8,8 @@ import member.service.SignUpMember;
 
 public class IndexMain {
 	private Scanner scan = new Scanner(System.in);
-	
+	int num;
 	public void menu() {
-		int num;
 		Member member = null;
 
 		while(true) {
@@ -19,21 +19,24 @@ public class IndexMain {
 			System.out.println("	3. CineManager 종료");
 			System.out.println("*****************************************");
 			System.out.print("번호 입력 : ");
-			num = scan.nextInt();
-			scan.nextLine(); // 개행 문자 제거
-			System.out.println();
-
-			if (num == 1) {
-				member = new SignUpMember();
-				member.execute();
-			} else if (num == 2) {
-				member = new SignInMember();
-				member.execute();
-			} else if (num == 3) {
-				break;
-			} else {
-				System.out.println("잘못된 선택입니다. 다시 선택해주세요.\n");
-			}	
+			try {
+				num = scan.nextInt();
+				scan.nextLine();
+				if (num == 1) {
+					member = new SignUpMember();
+					member.execute();
+				} else if (num == 2) {
+					member = new SignInMember();
+					member.execute();
+				} else if (num == 3) {
+					break;
+				} else {
+					System.out.println("잘못된 선택입니다. 다시 선택해주세요.\n");
+				}
+			}catch(InputMismatchException e) {
+				System.out.println("'숫자'형태만 입력하세요.");
+				scan.nextLine();
+			}
 
 		}
 	}
